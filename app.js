616a6d265903e7004a4d6613ca6ed0481e1c3f29
Bohdan <grul55@gmail.com>
2013-04-09 18:36:00 +0300

    initial test

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
 	, http = require('http')
 	, path = require('path');
@@ -11,7 +6,6 @@ var app = express();
 
 app.configure(function(){
 	app.set('port', process.env.PORT || 3000);
-	//app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
